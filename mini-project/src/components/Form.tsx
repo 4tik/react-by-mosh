ac3db0fc303479d6e78bf1e167b5ff5fc87e1656
Syntax.Error <atikrah2010@gmail.com>
2025-04-28 03:57:11 +0600

    Add form validation using react-hook-form and zod; update Form component to handle user input and display errors

--- mini-project/src/components/Form.tsx
+++ mini-project/src/components/Form.tsx
@@ -1,22 +1,55 @@
-import React from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  name: z.string().min(3),
+  age: z.number({ invalid_type_error: "Age field is required." }).min(18),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input type="text" id="name" className="form-control" />
+        <input
+          type="text"
+          {...register("name", { required: true })}
+          id="name"
+          className="form-control"
+        />
+        {errors.name && (
+          <span className="text-danger">{errors.name?.message}</span>
+        )}
       </div>
 
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input type="number" id="age" className="form-control" />
+        <input
+          type="number"
+          {...register("age", { valueAsNumber: true })}
+          id="age"
+          className="form-control"
+        />
+        {errors.age && (
+          <span className="text-danger">{errors.age?.message}</span>
+        )}
       </div>
-      <button className="btn btn-primary" type="submit">
+      <button disabled={!isValid} className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
